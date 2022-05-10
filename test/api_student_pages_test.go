c412896d8c772be7f178f0f9468fafd8b7caef3e
Danil Bubnov <dkbubnov@edu.hse.ru>
2022-05-10 17:48:01 +0500

    Add student api pages tests

---
+++ test/api_student_pages_test.go
@@ -0,0 +1,139 @@
+package test
+
+import (
+	myhwproj "github.com/KaperD/HSE-SD-MyHwProj/internal"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestGetHomeworkPageStudentOk(t *testing.T) {
+	timeExample, err := time.Parse(time.RFC3339, "2022-05-01T17:32:28+03:00")
+	require.Nil(t, err)
+	initialHomeworks := []myhwproj.Homework{
+		{
+			Id:                  0,
+			Name:                "Test",
+			PublicationDatetime: timeExample.Add(-time.Hour),
+			Check:               "./gradlew test",
+			Task:                "Task",
+			Deadline:            timeExample.Add(3 * time.Hour),
+		},
+	}
+	initialSubmissions := []myhwproj.Submission{
+		{
+			Id:         0,
+			HomeworkId: 0,
+			Datetime:   timeExample,
+			Solution:   "1",
+			Mark:       0,
+			Comment:    "",
+		},
+		{
+			Id:         1,
+			HomeworkId: 0,
+			Datetime:   timeExample.Add(time.Hour),
+			Solution:   "2",
+			Mark:       3,
+			Comment:    "Hello",
+		},
+	}
+	server := CreateServer(initialHomeworks, initialSubmissions)
+	defer server.Close()
+
+	data, code, err := Get(server, "/student/homeworks/0")
+	require.Nil(t, err)
+
+	require.Equal(t, http.StatusOK, code)
+
+	expectedHtmlData, err := ioutil.ReadFile("./test/resources/get_homework_student.html")
+	require.Nil(t, err)
+	expectedHtml := string(expectedHtmlData)
+
+	require.Equal(t, expectedHtml, string(data))
+}
+
+func TestGetHomeworksPageStudentOk(t *testing.T) {
+	timeExample, err := time.Parse(time.RFC3339, "2022-05-01T17:32:28+03:00")
+	require.Nil(t, err)
+	initialHomeworks := []myhwproj.Homework{
+		{
+			Id:                  0,
+			Name:                "Test",
+			PublicationDatetime: timeExample.Add(-time.Hour),
+			Check:               "./gradlew test",
+			Task:                "Task",
+			Deadline:            timeExample.Add(3 * time.Hour),
+		},
+		{
+			Id:                  1,
+			Name:                "Test2",
+			PublicationDatetime: timeExample.Add(-time.Hour),
+			Check:               "./gradlew test2",
+			Task:                "Task2",
+			Deadline:            timeExample.Add(4 * time.Hour),
+		},
+	}
+	var initialSubmissions []myhwproj.Submission
+	server := CreateServer(initialHomeworks, initialSubmissions)
+	defer server.Close()
+
+	data, code, err := Get(server, "/student/homeworks")
+	require.Nil(t, err)
+
+	require.Equal(t, http.StatusOK, code)
+
+	expectedHtmlData, err := ioutil.ReadFile("./test/resources/get_homeworks_student.html")
+	require.Nil(t, err)
+	expectedHtml := string(expectedHtmlData)
+
+	require.Equal(t, expectedHtml, string(data))
+}
+
+func TestGetSubmissionPageStudentOk(t *testing.T) {
+	timeExample, err := time.Parse(time.RFC3339, "2022-05-01T17:32:28+03:00")
+	require.Nil(t, err)
+	initialHomeworks := []myhwproj.Homework{
+		{
+			Id:                  0,
+			Name:                "Test",
+			PublicationDatetime: timeExample.Add(-time.Hour),
+			Check:               "./gradlew test",
+			Task:                "Task",
+			Deadline:            timeExample.Add(3 * time.Hour),
+		},
+	}
+	initialSubmissions := []myhwproj.Submission{
+		{
+			Id:         0,
+			HomeworkId: 0,
+			Datetime:   timeExample,
+			Solution:   "1",
+			Mark:       0,
+			Comment:    "",
+		},
+		{
+			Id:         1,
+			HomeworkId: 0,
+			Datetime:   timeExample.Add(time.Hour),
+			Solution:   "2",
+			Mark:       3,
+			Comment:    "Hello",
+		},
+	}
+	server := CreateServer(initialHomeworks, initialSubmissions)
+	defer server.Close()
+
+	data, code, err := Get(server, "/student/submissions/1")
+	require.Nil(t, err)
+
+	require.Equal(t, http.StatusOK, code)
+
+	expectedHtmlData, err := ioutil.ReadFile("./test/resources/get_submission_student.html")
+	require.Nil(t, err)
+	expectedHtml := string(expectedHtmlData)
+
+	require.Equal(t, expectedHtml, string(data))
+}
